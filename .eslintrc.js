953ded6354a200425cf2cb3e30a6bf5448257c41
Aleksandar Dimitrov <aleksandar.dimitrov@and.digital>
2020-07-20 13:46:57 +0300

    change eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      jest: true
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+    ],
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly",
+      module: false,
+      process: false,
+    },
+    parser: "babel-eslint",
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "react/prop-types": 0,
+      "react/state-in-constructor": "off",
+    },
+  };
