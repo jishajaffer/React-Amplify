e6f7ca38b2ca5ef00f543ee592e6bb07402e8ac3
Michael Wimbledon <michael@Michaels-MBP.home>
2020-07-21 17:04:34 +0100

    added button handling functions

--- src/Pages/Post/Post.js
+++ src/Pages/Post/Post.js
@@ -6,22 +6,43 @@ const Post = (props) => {
   const postId = props.match.params.id;
   let post = postsApi.getPostsById(postId);
 
+  const handleEdit = (id) => {
+    //update path to relevant page (edit) and pass ID
+    window.location = "/";
+  };
+
+  const handleCancel = () => {
+    //update path to relevant page (home)
+    window.location = "/";
+  };
+
+  const handleDelete = (id) => {
+    //Call api delete article
+    //Update path to relevant page (home)
+    window.location = "/";
+  };
+
   return (
     <div>
       <table className="table table-striped">
         <thead>
           <tr>
-            <th scope="col" data-testid="titleId">{post.title}</th>
+            <th scope="col" data-testid="titleId">
+              {post.title}
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>
-              <p className="text-justify" data-testid="contentId">{post.content}</p>
+              <p className="text-justify" data-testid="contentId">
+                {post.content}
+              </p>
             </td>
             <td>
               {post.image ? (
-                <img data-testid="imageId"
+                <img
+                  data-testid="imageId"
                   src={post.image}
                   className="rounded mx-auto d-block"
                   alt={post.image}
@@ -38,13 +59,28 @@ const Post = (props) => {
             <>
               <tr>
                 <td>
-                  <button type="button" className="btn float-right btn-success mr-2" data-testid="buttonEdit">
+                  <button
+                    type="button"
+                    onClick={()=>handleEdit(postId)}
+                    className="btn float-right btn-success mr-2"
+                    data-testid="buttonEdit"
+                  >
                     Edit
                   </button>
-                  <button type="button" className="btn float-right btn-success mr-2" data-testid="buttonCancel">
+                  <button
+                    type="button"
+                    onClick={handleCancel}
+                    className="btn float-right btn-success mr-2"
+                    data-testid="buttonCancel"
+                  >
                     Cancel
                   </button>
-                  <button type="button" className="btn float-right btn-success mr-2" data-testid="buttonDelete"> 
+                  <button
+                    type="button"
+                    onClick={()=>handleDelete(postId)}
+                    className="btn float-right btn-success mr-2"
+                    data-testid="buttonDelete"
+                  >
                     Delete
                   </button>
                 </td>
