53827b84492d075d67d5f449658da0e7a48c1ccf
Sebastian Blackall <sebastian.blackall@and.digital>
2020-07-22 16:45:56 +0100

    update article card to be more in line with new design spec

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -49,22 +49,18 @@ import { Link } from "react-router-dom";
 function ArticleCard(props) {
   const { articleId, articleTitle, articleContent, articleCategory, highlighted } = props;
   return (
-    <div className={`card mb-3 shadow-sm ${highlighted && "border-danger"}`} >
-      <div className="row no-gutters">
-        <div className="col-md-4 p-4">
-          <svg className="bd-placeholder-img card-img-top" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+    <div className={`card mb-4 shadow-sm ${highlighted && "border-danger"}`}>
+      <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22538%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20538%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1737720013b%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A27pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1737720013b%22%3E%3Crect%20width%3D%22538%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22178%22%20y%3D%22124.5%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"/>
+      <div className="card-body">
+        <h5 className="card-title">{articleTitle}</h5>
+        <p className="card-text">{articleContent.length > 200 ? `${articleContent.substring(0, 200)}...` : articleContent.substring(0, 200)}</p>
+        <div className="d-flex justify-content-between align-items-center">
+          <Link className="btn btn-sm btn-secondary" to={`/home/?sortByCategory=${articleCategory}`}>{articleCategory}</Link>
+          <div className="btn-group float-right">
+            <Link className="btn btn-sm btn-outline-secondary" to={`/article/${articleId}`} >Read More</Link>
+            <Link className="btn btn-sm btn-outline-secondary" to="/article/1" >Edit</Link>
+          </div> 
         </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{articleTitle}</h5>
-            <p className="card-text">{articleContent.length > 200 ? `${articleContent.substring(0, 200)}...` : articleContent.substring(0, 200)}</p>
-            <div className="btn-group float-right">
-              <Link className="btn btn-sm btn-outline-secondary" to={`/article/${articleId}`} >Read More</Link>
-              <Link className="btn btn-sm btn-outline-secondary" to="/article/1" >Edit</Link>
-            </div>
-          </div>
-        </div>
-        <span className="border border-secondary rounded p-1 ml-4 mt-3 mb-4">{`Category: ${articleCategory}`}</span>
       </div>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,27 +11,27 @@ function Home(props) {
   const articles = sortByCategory === "All" ? fakeArticleService.getArticles() : fakeArticleService.getArticlesByCategory(sortByCategory);
   const highlightedArticles = fakeArticleService.getHighlightedArticles(articles);
   const nonhighlightedArticles = fakeArticleService.getNonHighlightedArticles(articles);
-  
+
   console.log("foo", sortByCategory);
-  
+
   const handleFilterCategory = ({ currentTarget: { value } }) => {
     console.log(value);
     //window.location = `/home/?sortByCategory=${value}`;
     props.history.push(`/home/?sortByCategory=${value}`);
   };
-  
+
   const chunkArticles = (articles, size) => {
     let index = 0;
     let results = [];
-  
+
     while (index < articles.length) {
       results.push(articles.slice(index, size + index));
       index += size;
     }
-  
+
     return results;
   };
-  
+
   return (
     <>
       <h1>Home - imagine a nav bar above</h1>
@@ -49,53 +49,57 @@ function Home(props) {
         (
           <section className="highlighted d-block pb-4">
             <h6 className="p-3 pb-4">Highlighted</h6>
-            {chunkArticles(highlightedArticles, 2).map((articleGroup, groupIndex) => {
-              return (
-                <div className="row ml-3 mr-3" key={groupIndex}>
-                  {articleGroup.map((article, articleIndex) => {
-                    const { id: articleId, title: articleTitle, content: articleContent, category: articleCategory } = article;
-                    return (
-                      <div className="col-lg-6" key={articleIndex}>
-                        <ArticleCard
-                          articleId={articleId}
-                          articleTitle={articleTitle}
-                          articleContent={articleContent}
-                          articleCategory={articleCategory}
-                          highlighted={true}
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
+            <div className="container">
+              {chunkArticles(highlightedArticles, 2).map((articleGroup, groupIndex) => {
+                return (
+                  <div className="row" key={groupIndex}>
+                    {articleGroup.map((article, articleIndex) => {
+                      const { id: articleId, title: articleTitle, content: articleContent, category: articleCategory } = article;
+                      return (
+                        <div className="col-md-6" key={articleIndex}>
+                          <ArticleCard
+                            articleId={articleId}
+                            articleTitle={articleTitle}
+                            articleContent={articleContent}
+                            articleCategory={articleCategory}
+                            highlighted={true}
+                          />
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+            </div>
           </section>
         )
       }
 
       <section className="non-highlighted d-block pb-4 mt-5">
-        {chunkArticles(nonhighlightedArticles, 2).map((articleGroup, groupIndex) => {
-          return (
-            <div className="row ml-3 mr-3" key={groupIndex}>
-              {articleGroup.map((article, articleIndex) => {
-                const { id: articleId, title: articleTitle, content: articleContent, category: articleCategory } = article;
-                return (
-                  <div className="col-md-6" key={articleIndex}>
-                    <ArticleCard
-                      articleId={articleId}
-                      articleTitle={articleTitle}
-                      articleContent={articleContent}
-                      articleCategory={articleCategory}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+        <div className="container">
+          {chunkArticles(nonhighlightedArticles, 2).map((articleGroup, groupIndex) => {
+            return (
+              <div className="row ml-3 mr-3" key={groupIndex}>
+                {articleGroup.map((article, articleIndex) => {
+                  const { id: articleId, title: articleTitle, content: articleContent, category: articleCategory } = article;
+                  return (
+                    <div className="col-md-6" key={articleIndex}>
+                      <ArticleCard
+                        articleId={articleId}
+                        articleTitle={articleTitle}
+                        articleContent={articleContent}
+                        articleCategory={articleCategory}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </section>
 
-      
+
     </>
   );
 }
