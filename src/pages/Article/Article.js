ceb5e5e244062c6fd71c10e1f094474f016421f7
Ravindra Vedanthappa <ravindra@Ravindras-MBP.broadband>
2020-07-23 11:49:33 +0100

    PR comments

--- src/pages/Article/Article.js
+++ src/pages/Article/Article.js
@@ -11,86 +11,62 @@ const Article = (props) => {
     window.location = "/";
   };
 
-  const handleCancel = () => {
-    //update path to relevant page (home)
-    window.location = "/";
-  };
-
   const handleDelete = () => {
     //Call api delete article
     //Update path to relevant page (home)
-    window.location = "/";
+    window.location = "/home";
   };
 
   return (
-    <div>
+    <div className="container">
       {article && (
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col" data-testid="titleId">
-                {article.title}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <p className="text-justify" data-testid="contentId">
-                  {article.content}
-                </p>
-              </td>
-              <td>
-                <img
-                  data-testid="imageId"
-                  src={article.image}
-                  className="rounded mx-auto d-block"
-                  alt={article.image}
-                />
+        <>
+          {article.image && (
+            <div class="jumbotron">
+              <img
+                data-testid="imageId"
+                src={article.image}
+                className="rounded mx-auto d-block"
+                alt={article.image}
+              />
+            </div>
+          )}
+          <div className="row">
+            <div className="col-md-4">
+              <h3>{article.category}</h3>
+            </div>
+            <div className="col-md-8">
+              <h1>{article.title}</h1>
+              <p className="text-justify" data-testid="contentId">
+                {article.content}
+              </p>
+            </div>
+          </div>{" "}
+        </>
+      )}
 
-                <></>
-              </td>
-            </tr>
+      {article && isAdmin && (
+        <div className="row">
+          <div className="col-md-12">
+            <button
+              type="button"
+              onClick={() => handleEdit(articleId)}
+              className="btn float-right btn-success mr-2"
+              data-testid="buttonEdit"
+            >
+              Edit
+            </button>
 
-            <tr>
-              <td>{article.category}</td>
-            </tr>
-
-            {isAdmin && (
-              <>
-                <tr>
-                  <td>
-                    <button
-                      type="button"
-                      onClick={() => handleEdit(articleId)}
-                      className="btn float-right btn-success mr-2"
-                      data-testid="buttonEdit"
-                    >
-                      Edit
-                    </button>
-                    <button
-                      type="button"
-                      onClick={handleCancel}
-                      className="btn float-right btn-success mr-2"
-                      data-testid="buttonCancel"
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      type="button"
-                      onClick={() => handleDelete(articleId)}
-                      className="btn float-right btn-success mr-2"
-                      data-testid="buttonDelete"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              </>
-            )}
-            <></>
-          </tbody>
-        </table>
+            <button
+              type="button"
+              onClick={() => handleDelete(articleId)}
+              className="btn float-right btn-success mr-2"
+              data-testid="buttonDelete"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
