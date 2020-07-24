10fb345a87fc0ca9bdafbd05c7e2ff5e5b3d1c96
Sebastian Blackall <sebastian.blackall@and.digital>
2020-07-24 10:33:59 +0100

    set background color to pink, add whitebackground to cards on homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -34,45 +34,43 @@ function Home(props) {
     <>
       <h1>Home - imagine a nav bar above</h1>
 
-      <div className="d-block bg-pink">
-        <div className="container">
-          <div className="row py-4">
-            <div className="col-12">
-              <div className="d-flex rounded p-2 text-dark bg-light shadow-sm justify-content-between">
-                <Link className="btn btn-secondary" to="/article/create">Create New Article</Link>
-                <select className="custom-select align-self-center ml-2" onChange={handleFilterCategory} value={sortByCategory}>
-                  <option value="All">All</option>
-                  {fakeCategoryService.getCategories().map((category, index) => (
-                    <option key={`category-${index}`} value={category}>{category}</option>
-                  ))}
-                </select>
-              </div>
+      <div className="container">
+        <div className="row py-4">
+          <div className="col-12">
+            <div className="d-flex rounded p-2 text-dark bg-light shadow-sm justify-content-between">
+              <Link className="btn btn-secondary" to="/article/create">Create New Article</Link>
+              <select className="custom-select align-self-center ml-2" onChange={handleFilterCategory} value={sortByCategory}>
+                <option value="All">All</option>
+                {fakeCategoryService.getCategories().map((category, index) => (
+                  <option key={`category-${index}`} value={category}>{category}</option>
+                ))}
+              </select>
             </div>
           </div>
-          <section className="article-section">
-            {chunkArticles(sortedArticles, 2).map((articleGroup, groupIndex) => {
-              return (
-                <div className="row" key={groupIndex}>
-                  {articleGroup.map((article, articleIndex) => {
-                    const { id: articleId, image: articleImage, title: articleTitle, content: articleContent, category: articleCategory, highlighted } = article;
-                    return (
-                      <div className="col-md-6 mb-4" key={articleIndex}>
-                        <ArticleCard
-                          articleId={articleId}
-                          articleImage={articleImage}
-                          articleTitle={articleTitle}
-                          articleContent={articleContent}
-                          articleCategory={articleCategory}
-                          highlighted={highlighted}
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
-          </section>
         </div>
+        <section className="article-section bg-light p-2 rounded shadow-sm">
+          {chunkArticles(sortedArticles, 2).map((articleGroup, groupIndex) => {
+            return (
+              <div className="row" key={groupIndex}>
+                {articleGroup.map((article, articleIndex) => {
+                  const { id: articleId, image: articleImage, title: articleTitle, content: articleContent, category: articleCategory, highlighted } = article;
+                  return (
+                    <div className="col-md-6 mb-4" key={articleIndex}>
+                      <ArticleCard
+                        articleId={articleId}
+                        articleImage={articleImage}
+                        articleTitle={articleTitle}
+                        articleContent={articleContent}
+                        articleCategory={articleCategory}
+                        highlighted={highlighted}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </section>
       </div>
     </>
   );
