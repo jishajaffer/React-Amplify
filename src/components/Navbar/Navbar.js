ce683265855b98ded3b5cdaa03be59044eac66f5
Michael Wimbledon <michael@Michaels-MBP.home>
2020-07-27 12:03:49 +0100

    changed user icon and removed context

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
+import React /*{ useContext }*/ from "react";
 import logo from "../../images/summercamp.png";
-import userContext from "../../context/userContext";
-import womanPic from "../../images/woman.png"
+//import userContext from "../../context/userContext";
+import womanPic from "../../images/woman.png";
 
 import "./Navbar.css";
 
 function Navbar() {
-  const { user } = useContext(userContext);
+  //const { user } = useContext(userContext);
 
   return (
     <nav className="navbar bg-white navbar-expand">
