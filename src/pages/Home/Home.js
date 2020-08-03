3798551847efdc73f242a72b5fed05cd365a4214
Aleksandar Dimitrov <aleksandar.dimitrov@and.digital>
2020-08-03 13:45:23 +0300

    add check to make sure requests are not sent before the user is set

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,24 +15,29 @@ function Home(props) {
   const [categories, setCategories] = useState([]);
 
   const sortArticles = (articles) => {
-    articles.sort((a, b) => (a.dateCreated > b.dateCreated) ? -1 : ((a.dateCreated < b.dateCreated) ? 1 : 0));
+    articles.sort((a, b) =>
+      a.dateCreated > b.dateCreated ? -1 : a.dateCreated < b.dateCreated ? 1 : 0
+    );
     // Sorting by subtracting a boolean works because true - false === 1, false - true === -1 and true - true === 0
     articles.sort((a, b) => b.highlighted - a.highlighted);
   };
 
   const filterArticlesByCategory = (articles, category) => {
-    return articles.filter((article) => article.articleCategories[0].category.categoryName === category);
+    return articles.filter(
+      (article) =>
+        article.articleCategories[0].category.categoryName === category
+    );
   };
 
   const initArticles = () => {
     if (filterCategory === "All") {
-      articleService.getArticles().then(response => {
+      articleService.getArticles().then((response) => {
         const { data: articles } = response;
         sortArticles(articles);
         setArticles(articles);
       });
     } else {
-      articleService.getArticlesByCategory(filterCategory).then(response => {
+      articleService.getArticlesByCategory(filterCategory).then((response) => {
         let { data: articles } = response;
         articles = filterArticlesByCategory(articles, filterCategory);
         sortArticles(articles);
@@ -43,15 +48,17 @@ function Home(props) {
 
   const initState = () => {
     initArticles();
-    categoryService.getCategories().then(response => {
+    categoryService.getCategories().then((response) => {
       const { data: categories } = response;
       setCategories(categories);
     });
   };
 
   useEffect(() => {
-    initState();
-    // eslint-disable-next-line 
+    if (Object.keys(props.user).length !== 0) {
+      initState();
+    }
+    // eslint-disable-next-line
   }, [filterCategory]);
 
   const handleFilterCategory = ({ currentTarget: { value } }) => {
@@ -75,24 +82,39 @@ function Home(props) {
       <div className="container">
         <div className="row mt-4">
           <div className="col-12">
-            <div className={`d-flex rounded p-2 text-dark bg-white shadow-sm ${!isAdmin ? "justify-content-end" : "justify-content-between"}`}>
-              {isAdmin &&
-                <Link className="btn btn-secondary" to="/articles/new">Create New Article</Link>
-              }
-              <select className="custom-select align-self-center ml-2" onChange={handleFilterCategory} value={filterCategory}>
+            <div
+              className={`d-flex rounded p-2 text-dark bg-white shadow-sm ${
+                !isAdmin ? "justify-content-end" : "justify-content-between"
+              }`}
+            >
+              {isAdmin && (
+                <Link className="btn btn-secondary" to="/articles/new">
+                  Create New Article
+                </Link>
+              )}
+              <select
+                className="custom-select align-self-center ml-2"
+                onChange={handleFilterCategory}
+                value={filterCategory}
+              >
                 <option value="All">All</option>
-                {
-                  categories.map((category) => {
-                    return <option key={`category-${category.categoryID}`} value={category.categoryName}>{category.categoryName}</option>;
-                  })
-                }
+                {categories.map((category) => {
+                  return (
+                    <option
+                      key={`category-${category.categoryID}`}
+                      value={category.categoryName}
+                    >
+                      {category.categoryName}
+                    </option>
+                  );
+                })}
               </select>
             </div>
           </div>
         </div>
         <section className="article-section my-4 bg-white p-2 rounded shadow-sm">
-          {articles.length > 0 ?
-            (chunkArticles(articles, 2).map((articleGroup, groupIndex) => {
+          {articles.length > 0 ? (
+            chunkArticles(articles, 2).map((articleGroup, groupIndex) => {
               return (
                 <div className="row" key={groupIndex}>
                   {articleGroup.map((article, articleIndex) => {
@@ -100,30 +122,40 @@ function Home(props) {
                     //const articleCategory = articleCategories[0].category.categoryName;
                     return (
                       <div className="col-md-6 mb-4" key={articleIndex}>
-                        <ArticleCard article={article} showEdit={isAdmin}/>
+                        <ArticleCard article={article} showEdit={isAdmin} />
                       </div>
                     );
                   })}
                 </div>
               );
-            })) : (
-              <div className="d-flex flex-column my-5">
-                <div className="row">
-                  <div className="col-md-12">
-                    <h2 className="text-center">Uh oh we couldn&apos;t find any articles...<br /> go back to viewing all articles?</h2>
-                  </div>
+            })
+          ) : (
+            <div className="d-flex flex-column my-5">
+              <div className="row">
+                <div className="col-md-12">
+                  <h2 className="text-center">
+                    Uh oh we couldn&apos;t find any articles...
+                    <br /> go back to viewing all articles?
+                  </h2>
                 </div>
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="text-center">
-                      <Link className="btn btn-secondary m-1" to="/?filterCategory=All">Yes</Link>
-                      <button className="btn btn-secondary m-1">No... I want to stare at this screen</button>
-                    </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-center">
+                    <Link
+                      className="btn btn-secondary m-1"
+                      to="/?filterCategory=All"
+                    >
+                      Yes
+                    </Link>
+                    <button className="btn btn-secondary m-1">
+                      No... I want to stare at this screen
+                    </button>
                   </div>
                 </div>
               </div>
-            )
-          }
+            </div>
+          )}
         </section>
       </div>
     </>
